59ac1906ab9bda1551a5fa2be8fe6a6a4591bf9f
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-27 19:11:15 +0600

    Inheritance and Class JS Files Done

--- class.js
+++ class.js
@@ -4,3 +4,34 @@ const products = [
     {id: 3, name: "Lenovo IdeaPad Laptop", price: 55000},
     {id: 4, name: "MacBook Air M2 Laptop", price: 1300000}
 ]
+
+
+// Has Some Properties, Method
+class Product {
+    country = "Bangladesh";
+    constructor(name) {
+        this.name = name;
+    }
+    speak(talk) {
+        console.log(`talking about ${talk}`);
+    }
+}
+
+const lenovo = new Product("Lenovo IdeaPad");
+console.log(lenovo)
+lenovo.speak("I am Lenovo Laptop of IdeaPad Series");
+
+class Teacher {
+    constructor(name, subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+    lecture() {
+        console.log("Sir is teaching Math")
+    }
+}
+
+const sabbir = new Teacher("Sabbir Sir", "Physics");
+console.log(sabbir);
+
+const rashid = new Teacher("Rashid", "English");
