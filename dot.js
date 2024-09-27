b827bd6a9d7a3caae94ad4e5a6894b7449509a5f
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-27 19:29:39 +0600

    Dot and This JS Files Done

---
+++ dot.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Kodom Ali", 
+    job: "Badam Khai",
+    3:"Third",
+    "Add-dress": "Kochu Khet"
+}
+
+const prop = "job"
+console.log(person.job)
+console.log(person["job"])
+console.log(person[prop])
+console.log(person["3"])
+console.log(person["Add-dress"]);
